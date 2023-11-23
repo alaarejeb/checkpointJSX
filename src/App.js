570adce4b93c4240af5431a3cef89789677b7bbf
Alla Rejeb <alaa-rejeb@hotmail.fr>
2023-11-23 15:52:42 +0100

    thrid

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import Image from "./Image";
 import UserImage from "./UserImage";
 import red2Image from "./Image/red2.png"; 
 import "./styles.css";
+// 
 
 const App = () => {
   var firstName = ""; 
+  // this is a prompt function for the user to write there name
 const dd=()=> {let a=(prompt("Your name"));
 firstName=a}
 dd()
 
   return (
+    /* bringing in all the components  */
     <div className="App">
       <Card className="product-card">
         <Image  className="product-image" />
@@ -24,8 +27,10 @@ dd()
           <Description  />
         </Card.Body>
       </Card>
+            {/* this will display the name or there  */}
       <p className="hello">Hello, {firstName ? firstName : "there"}!</p>
       {firstName && <UserImage image={red2Image} />}
+
       
       
     </div>
